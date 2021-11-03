12f216a62254c71ca185394cf977535caae46855
Greg <gbrewton1980@gmail.com>
2021-11-03 14:04:09 -0600

    uses json formatting for express router

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ colors.enable();
 
 const port = process.env.PORT || 5000;
 
+server.use(express.json());
+
 server.listen(port, () =>
   console.log(`\n** Server listening on port ${port} **\n`.rainbow)
 );
