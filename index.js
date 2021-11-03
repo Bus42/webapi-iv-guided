af4b79104990a3e21a68072cd027bfe70c59dec0
Greg <gbrewton1980@gmail.com>
2021-11-03 13:50:58 -0600

    Adds .env

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server.js');
+require("dotenv").config();
+const server = require("./api/server.js");
+const colors = require("colors");
+colors.enable();
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
-});
+const port = process.env.PORT || 5000;
+
+server.listen(port, () =>
+  console.log(`\n** Server listening on port ${port} **\n`.rainbow)
+);
